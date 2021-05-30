c77bf56d51ac54aa4d8035874d2ef02dc063da12
Robin Jacobs <jacobsr@ethz.ch>
2021-05-30 18:04:41 +0200

    added code for curiosity v2

--- navrep/scripts/train_gym_e2enavreptrainenv_curiosity_v2.py
+++ navrep/scripts/train_gym_e2enavreptrainenv_curiosity_v2.py
@@ -9,7 +9,8 @@ from navrep.envs.e2eenv import E2ENavRepEnv, E2ENavRepEnvCuriosity
 from navrep.tools.sb_eval_callback import NavrepEvalCallback
 from navrep.tools.commonargs import parse_common_args
 
-from navrep.models.curiosity import CuriosityWrapper
+#from navrep.models.curiosity import CuriosityWrapper
+from navrep.models.curiosity_v2 import CuriosityWrapper
 
 from navrep.envs.navreptrainencodedenv import *
 
@@ -25,7 +26,7 @@ if __name__ == "__main__":
         LOGDIR = "/tmp/navrep/logs/gym"
     START_TIME = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
     CONTROLLER_ARCH = "_{}_C{}".format(ARCH, _C)
-    LOGNAME = "CURIOSITY_e2enavreptrainenv_" + START_TIME + "_PPO" + "_E2E" + CONTROLLER_ARCH
+    LOGNAME = "CURIOSITY_V2_e2enavreptrainenv_" + START_TIME + "_PPO" + "_E2E" + CONTROLLER_ARCH
     LOGPATH = os.path.join(LOGDIR, LOGNAME + ".csv")
     MODELPATH = os.path.join(DIR, LOGNAME + "_ckpt")
     MODELPATH2 = os.path.join(DIR, "e2enavreptrainenv_latest_PPO_ckpt")
@@ -39,7 +40,7 @@ if __name__ == "__main__":
     if TRAIN_STEPS is None:
         TRAIN_STEPS = 5 * MILLION
 
-    N_ENVS = 4
+    N_ENVS = 1 # 4 in best run 6 originally
     #if args.debug:
     #    env = DummyVecEnv([lambda: CuriosityWrapper(
     #        E2ENavRepEnvCuriosity(silent=True, scenario='train'))]*N_ENVS)
@@ -51,22 +52,26 @@ if __name__ == "__main__":
     #env = SubprocVecEnv([lambda: CuriosityWrapper(E2ENavRepEnvCuriosity(silent=True, scenario='train'))]*N_ENVS, start_method='spawn')
     #env = SubprocVecEnv([lambda: CuriosityWrapper(E2ENavRepEnv(silent=True, scenario='train'))]*N_ENVS, start_method='spawn')
     
-    env = SubprocVecEnv([lambda: CuriosityWrapper(NavRepTrainEncodedEnvCuriosity(backend='VAE_LSTM', encoding='V_ONLY', silent=True, scenario='train'),
-                        use_gpu=False, feature_tf=None)]*N_ENVS, start_method='spawn')
+    # use this before in best run so far
+    #env = SubprocVecEnv([lambda: CuriosityWrapper(NavRepTrainEncodedEnvCuriosity(backend='VAE_LSTM', encoding='VM', silent=True, scenario='train'),
+    #                    use_gpu=False, feature_tf=None)]*N_ENVS, start_method='spawn')
+
+    env = SubprocVecEnv([lambda: CuriosityWrapper(NavRepTrainEnvCuriosity(silent=True, scenario='train'),
+                        use_gpu=False, feature_tf=None, obsv_encoding='rings2d')]*N_ENVS, start_method='spawn')
 
 
-    #eval_env = E2ENavRepEnv(silent=True, scenario='train')
-    eval_env = NavRepTrainEncodedEnv(backend='VAE_LSTM', encoding='V_ONLY', silent=True, scenario='train')
+    eval_env = E2ENavRepEnv(silent=True, scenario='train')
+    #eval_env = NavRepTrainEncodedEnv(backend='VAE_LSTM', encoding='VM', silent=True, scenario='train')
 
     def test_env_fn():  # noqa
-        #return E2ENavRepEnv(silent=True, scenario='test')
-        return NavRepTrainEncodedEnv(backend='VAE_LSTM', encoding='V_ONLY', silent=True, scenario='test')
+        return E2ENavRepEnv(silent=True, scenario='test')
+        #return NavRepTrainEncodedEnv(backend='VAE_LSTM', encoding='VM', silent=True, scenario='test')
     
     cb = NavrepEvalCallback(eval_env, test_env_fn=test_env_fn,
                             logpath=LOGPATH, savepath=MODELPATH, verbose=1, render=args.render)
     
-    model = PPO2(MlpPolicy, env, verbose=0)
-    #model = PPO2(CustomPolicy, env, verbose=0)
+    #model = PPO2(MlpPolicy, env, verbose=0)
+    model = PPO2(CustomPolicy, env, verbose=0)
 
     model.learn(total_timesteps=TRAIN_STEPS+1, callback=cb)
     obs = env.reset()
